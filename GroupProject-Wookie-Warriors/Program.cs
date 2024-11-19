eaa6dfd37a2e7a14e9cbaf9d3b9e77bc03ce93f3
Conixen <leon.json57@gmail.com>
2024-11-19 11:04:53 +0100

    Add project files.

---
+++ GroupProject-Wookie-Warriors/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GroupProject_Wookie_Warriors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Group project - Wookie Warriors - SUT24
+            // Filip, Leon, Tim, Shokran, Simon
+
+            // Skriv inget i Master, skapa en ny branch        }
+        }
+    }
