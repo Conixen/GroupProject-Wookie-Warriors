a2e2b5163d6913a4814b7e1d05df65afbc3f67f8
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2024-11-20 13:21:46 +0100

    Login class parkodning

---
+++ GroupProject-Wookie-Warriors/Login.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace GroupProject_Wookie_Warriors
+{
+    public class Login : User
+    {
+        public void Menu()
+        {
+            Console.WriteLine("Welcome login");
+            int user;
+            int adminuser;
+            user = int.Parse(Console.ReadLine());
+            adminuser = int.Parse(Console.ReadLine());
+
+            if(user == 1)
+            {
+                LoginUser();
+            }
+
+            if (adminuser == 2 )
+            {
+                LoginAdmin();
+            }
+
+        }
+
+        public void LoginUser()
+        {
+
+        }
+
+        public void LoginAdmin()
+        {
+
+        }
+
+       
+    }
+}
