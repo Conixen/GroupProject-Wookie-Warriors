bd145ba85f3488f53c5a421af81bde7e9896015c
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2024-11-20 13:21:46 +0100

    Admin-class Method to create usersLogin class parkodning

--- GroupProject-Wookie-Warriors/Admin.cs
+++ GroupProject-Wookie-Warriors/Admin.cs
@@ -8,6 +8,17 @@ namespace GroupProject_Wookie_Warriors
 {
     public class Admin : User
     {
+        List<string> Permissions {  get; set; }
+
+        public Admin(string username, string password, List<string> permissions)
+        {
+            Permissions = permissions;
+        }
+
+        public override void CreateUser()
+        {
+
+        }
 
     }
 }
