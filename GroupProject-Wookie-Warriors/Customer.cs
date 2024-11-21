5d57807bb3bb40f0b98001565737d2ce31b82b22
shobah24 <shokran.bahram14@gmail.se>
2024-11-21 19:14:19 +0100

    methods for customer

--- GroupProject-Wookie-Warriors/Customer.cs
+++ GroupProject-Wookie-Warriors/Customer.cs
@@ -2,6 +2,46 @@
 {
     class Customer : User
     {
-       
+       static void CustomerAccounts()
+       {
+          // konto och saldo 
+       }
+
+       static void TransferToAccount()
+       {
+         // överföring mellan två konton   
+       }
+
+       static void TransferToOtherCustomer()
+       {
+         // överföring till andra användare   
+       }
+
+       static void TransferLog()
+       {
+         // logg på alla överföringar mm.... 
+       }
+
+       static void AddNewAccount()
+       {
+         // öppna nya konton   
+       }
+
+       static void AccountInOtherCurrency()
+       {
+          // konto i annan valuta  
+       }
+        
+       static void OpenSavingAccounts()
+       {
+          //  öppna sparkonto och se ränta på insättning
+       }
+
+       static void LoanAndInterest()
+       {
+         //lån + ränta   
+       } 
+        
+
     }
 }
