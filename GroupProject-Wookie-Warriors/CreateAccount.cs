290e2ec302031c2ec4cccb9ad5cb7e04a79c11e7
filip <filip@FILIP>
2024-11-29 21:28:55 +0100

    Added accounts for every user

--- GroupProject-Wookie-Warriors/CreateAccount.cs
+++ GroupProject-Wookie-Warriors/CreateAccount.cs
@@ -9,12 +9,28 @@ namespace GroupProject_Wookie_Warriors
     public class CreateAccount : Login
     { 
         //Initiera konton ock skapa konton
-        static void CreateAccount(string username, decimal[] accounts)
+        static void CreateAccounts(string username, decimal[] accounts)
         {
             switch (username)
             {
                 case "Filip":
                     accounts[0] = 300.30m;
+                    accounts[1] = 100.25m;
+                    break;
+                case "Simon":
+                    accounts[0] = 20.24m;
+                    break;
+                case "Tim":
+                    accounts[0] = 200.10m;
+                    accounts[1] = 60.65m;
+                    break;
+                case "Shokran":
+                    accounts[0] = 10000.53m;
+                    accounts[1] = 21645.65m;
+                    break;
+                case "Leon":
+                    accounts[0] = 2346.54m;
+                    accounts[1] = 23.65m;
                     break;
                 default:
                     for (int i = 0; i < accounts.Length; i++)
